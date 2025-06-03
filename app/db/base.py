47ed99149a454de1143971e6e25cada60e13c4b6
yggdrasilGit <168605322+yggdrasilGit@users.noreply.github.com>
2025-06-03 18:48:13 -0300

    foi feito todas as api em django tem que mudar a documentacao

--- app/db/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-from db.base import Base
-from db.models.sensor import SensorData  # Adicione todas as models aqui
-
-
-Base = declarative_base()
--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from api.routes import sensor
-
-app = FastAPI()
-
-app.include_router(sensor.router)
